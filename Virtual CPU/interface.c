08ff265ddbc9a2ea3a6ea2f29967950b6b162d59
B. Alfanous <b.alfanous@outlook.com>
2019-10-23 17:59:56 -0400

    coded help function

--- Virtual CPU/interface.c
+++ Virtual CPU/interface.c
@@ -8,6 +8,17 @@
 #include <stdlib.h>
 #include "vcpu.h"
 
-void help() {}
+void help() {
+	printf("\n\n\nd dump memory\n");
+	printf("g go – run the entire program\n");
+	printf("l load a file into memory\n");
+	printf("m memory modify\n");
+	printf("q quit\n");
+	printf("r display registers\n");
+	printf("t trace – execute one instruction\n");
+	printf("w write file\n");
+	printf("z reset all registers to zero\n");
+	printf("?, h display list of commands\n\n\n");
+}
 
 void run() {}
