d0bd694bfe34137e46325c075e07454c78630b6f
B. Alfanous <b.alfanous@outlook.com>
2019-10-24 16:53:57 -0400

    Coded writefile function

--- Virtual CPU/memFunc.c
+++ Virtual CPU/memFunc.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "vcpu.h"
 
  /*
@@ -55,4 +56,36 @@ int LoadFile(void* memory, unsigned int max) {
 	return read;
 }
 
-void WriteFile(void* memory) {}
+
+/*
+*   WriteFile is a function that prompts the user to enter file name.
+*   If the file already exists, it will be overridden.
+*   The user will be asked to enter the number of bytes to be written to the file.
+*   A file will be created, and the data entered will be written to the file from the buffer.
+*   Checks if file could be opened; if not, it returns -1.
+*/
+void WriteFile(void* memory) {
+	FILE* fp;
+	char fileName[32];
+	short int byte = 0, write = 0;
+	char ch;
+
+	printf("Enter a file name to be created: ");
+	scanf("%s", &fileName);
+	fp = fopen(fileName, "w");
+	if (fp == NULL) {
+		printf("\nCan't open file!!\n");
+		return -1;
+	}
+
+	printf("\nEnter number of bytes to be written to the file: ");
+	do {
+		scanf("%d", &byte);
+		if (byte > MEMORY_SIZE)
+			printf("\nError!!!...File size should be less than %dK Bytes!!\n", MEMORY_SIZE);
+	} while (byte > MEMORY_SIZE);
+
+	write = fwrite(memory, 1, byte, fp);
+	fclose(fp);
+	printf("Bytes number that have been written: %d bytes (%x hex).\n", write, write);
+}
