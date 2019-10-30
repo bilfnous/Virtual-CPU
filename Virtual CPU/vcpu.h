7b5f4b8e8b59b58e9c8c32e91d539b5a7dbc52c8
B. Alfanous <b.alfanous@outlook.com>
2019-10-29 20:57:56 -0400

    Added constants and iscarry function stub

--- Virtual CPU/vcpu.h
+++ Virtual CPU/vcpu.h
@@ -9,7 +9,9 @@
 
 
  // Constants 
-#define MEMORY_SIZE 16000
+#define MEMORY_SIZE 16000 // memory(buffer) size
+#define MAX32 4
+#define REG_NUM 16 // Number of registers
 
 
 // Functions Prototypes 
@@ -19,5 +21,6 @@ int LoadFile(char* memory, unsigned int max);
 void WriteFile(void* memory);
 void MemDump(void* memory, unsigned int offset, unsigned int length);
 void MemMod(void* memory, unsigned int address);
+int iscarry(unsigned long op1, unsigned long op2, unsigned C);
 
 #endif /* VCPU_H */
