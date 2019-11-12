5064aeaf81f781d4549fabbe5788895a40d9b3a2
B. Alfanous <b.alfanous@outlook.com>
2019-11-12 18:18:19 -0500

    defined instructions and condition codes

--- Virtual CPU/vcpu.h
+++ Virtual CPU/vcpu.h
@@ -23,6 +23,9 @@
 		In little-endian format, the processor stores the least 
 		significant byte of a word at the lowest-numbered byte, 
 		and the most significant byte at the highest-numbered byte.
+
+		Rd: Destination register
+		Rn: An operand in a register for an arithmetic operation.
 */
 
 // Constants (sizes)
@@ -39,14 +42,14 @@ bool CARRY;	// Carry Flag
 //#define IR_FLAG IR_FLAG //Flag to determine the active Instruction Regist
 
 // Registers
-int32_t IR;						//Instruction Register Combination of IR0 & IR1
+// CIR or IR Current Instruction Register - a temporary holding ground for the instruction that has just been fetched from memory
+int32_t IR;						// Instruction Register Combination of IR0 & IR1
 #define IR0 IR >> 0x10;			// Instruction Register 0
 #define IR1 IR & 0x000FFFF;		// Instruction Register 1
 int32_t MAR;					// Memory Address Register - the address in main memory that is currently being read or written
 int32_t MBR;					// Memory Buffer Register  - a two way register that holds data fetched from memory (and ready for the CPU to process) or data waiting to be stored in memory
 int32_t ALU;					// Arithmetic logic unit 
 int32_t CCR;					// ??? Condition Code Register CCR Flags
-int32_t CIR;					// ??? Current Instruction Register - a temporary holding ground for the instruction that has just been fetched from memory
 int32_t REG[REG_NUM];			// Registers Array
 
 /*
@@ -73,6 +76,37 @@ int32_t REG[REG_NUM];			// Registers Array
 	into the EPSR T-bit at reset and must be 1.
 */
 #define PC REG[0xF]; // Program Counter
+
+// Instructions
+/* Check if instruction fetched is equal to one of the predefined instructions. */
+#define LOAD_STORE	0x11 & IR0
+#define DATA_PROC	0x12 & IR0
+#define CON_BRANCH	0x13 & IR0
+#define UN_BRANCH	0x14 & IR0
+#define STOP		0x15 & IR0
+
+// Data processing instructions
+#define AND 0x0
+#define OR	0xC
+#define	EOR 0x1
+#define SUB 0x2
+#define ADD 0x4
+#define ADC 0x5
+#define SBC 0x6
+#define CMP 0xA
+#define CMN 0xB
+#define MOV 0xD
+
+// Condition Branch Codes
+#define EQ 0x0
+#define NE 0x1
+#define CS 0x2
+#define CC 0x3
+#define MI 0x4
+#define PL 0x5
+#define HI 0x8
+#define LS 0x9
+#define AL 0xE
 
 
 // Functions Prototypes 
